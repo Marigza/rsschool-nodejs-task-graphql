eeeb05e501db9e152b0c6e59ebcd124c28087730
Marigza <marigola@mail.ru>
2024-03-03 21:18:32 +0300

    feat: create base schemas and request

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { SomeKindOfSchema, createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const {prisma} = fastify
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,9 +14,46 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      //return await prisma.memberType.findMany()
+      const source = req.body.query;
+      const root = {
+        profiles: await prisma.profile.findMany(),
+        memberTypes: prisma.memberType.findMany(),
+        posts: prisma.post.findMany(),
+        users: prisma.user.findMany()
+      } 
+      return graphql({
+        schema: SomeKindOfSchema,
+        source: source,
+        rootValue: root,
+      })
+      //   .then((res) => {
+        
+      //   console.log('stupid girl from prisma', root.profiles.length )
+      //   console.log(`stupid girl res`, JSON.stringify(res))
+      //   //console.log(`stupid girl error`, JSON.stringify(res.errors))
+      //   return res;
+      // })
+      //   .catch(error => console.log(error))
+      //return {};
+      console.log('stupid girl source', source)
+      //console.log('stupid girl2', prisma.memberType.findMany())
     },
   });
 };
 
 export default plugin;
+
+
+// [
+//   {
+//     "message": "Invalid UUID.",
+//     "locations": [{ "line": 3, "column": 13 }],
+//     "path": ["memberTypes", 0, "id"]
+//   },
+//   {
+//     "message": "Invalid UUID.",
+//     "locations": [{ "line": 3, "column": 13 }],
+//     "path": ["memberTypes", 1, "id"]
+//   }
+// ]
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,7 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLBoolean, GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { UUIDType } from './types/uuid.js';
+import { Prisma } from '@prisma/client';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +21,99 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberTypeId = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: {
+      value: 'basic'
+    },
+    BUSINESS: {
+      value: 'business'
+    },
+  }
+})
+
+const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: {
+    id: {type: new GraphQLNonNull(UUIDType)},
+    name: {type: GraphQLString},
+    balance: {type: GraphQLFloat},
+
+    //profile: {type: ProfileType},
+    //posts: {type:  new GraphQLList(Post)},       
+    //userSubscribedTo: {type: new GraphQLList(SubscribersOnAuthors)},
+    //subscribedToUser: {type:new GraphQLList(SubscribersOnAuthors)}
+  }
+})
+
+const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: {type: MemberTypeId},
+    discount: {type: GraphQLFloat},
+    postsLimitPerMonth: {type: GraphQLInt},
+    //profiles: {type: new GraphQLList(ProfileType)}
+  }
+});
+
+const Post = new GraphQLObjectType({
+  name: 'PostType',
+  fields: {
+    id: {type: new GraphQLNonNull(UUIDType)},
+    title: {type: GraphQLString},
+    content: {type: GraphQLString},
+  }
+})
+
+const ProfileType = new GraphQLObjectType({
+  name: 'ProfileType',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: {type: GraphQLBoolean},
+    yearOfBirth: { type: GraphQLInt },
+    // user: {type: UserType },
+    // userId: { type: GraphQLString },
+    // memberType: { type: MemberType },
+    // memberTypeId: {type: GraphQLString}
+  }
+})
+
+// const rootMutation = new GraphQLObjectType({
+//   name: 'Mutation',
+//   fields: {}
+// });
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      // resolve(data) {
+      //   return data //какая то дата должна быть...
+      // }
+    },
+    posts: {
+      type: new GraphQLList(Post),
+     //resolve: ()=> {return Post}
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      //resolve: ()=> {return }
+    },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      //resolve: ()=>{ return true}
+    }
+  }
+})
+
+export const SomeKindOfSchema = new GraphQLSchema({
+  query: QueryType, 
+  
+}) 
+
+function getMemberTypes() {
+  return 
+}
